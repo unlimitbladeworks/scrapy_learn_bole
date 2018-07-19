a7dec0158affcbbd916547309ece843d3bbf7a7a
unlimitbladeworks <13121347080@sina.cn>
2018-07-19 23:02:29 +0800

    modify items Field and add args input_processor and output_processor

--- ArticleSpider/items.py
+++ ArticleSpider/items.py
@@ -4,8 +4,10 @@
 #
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
+import datetime
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
 
 
 class ArticlespiderItem(scrapy.Item):
@@ -14,10 +16,28 @@ class ArticlespiderItem(scrapy.Item):
     pass
 
 
+def add_jobbole(value):
+    return value + '-suyu'
+
+
+def date_convert(value):
+    try:
+        create_date = datetime.datetime.strptime(value, '%Y/%m%d').date()
+    except Exception as e:
+        create_date = datetime.datetime.now().date()
+    return create_date
+
+
 # 自定义文章Item
 class JobBoleArticleItem(scrapy.Item):
-    title = scrapy.Field()
-    create_date = scrapy.Field()
+    title = scrapy.Field(
+        # 传入item时做的预处理参数
+        input_processor=MapCompose(lambda x: x + '-jobbole', add_jobbole)
+    )
+    create_date = scrapy.Field(
+        input_processor=MapCompose(date_convert),
+        output_processor=TakeFirst()
+    )
     url = scrapy.Field()
     url_object_id = scrapy.Field()
     front_image_url = scrapy.Field()
