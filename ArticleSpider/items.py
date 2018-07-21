3292b1ad580eacf3c676818664ad735f553c515e
unlimitbladeworks <13121347080@sina.cn>
2018-07-21 21:41:20 +0800

    add some Fields processor and ArtcileItemLoad

--- ArticleSpider/items.py
+++ ArticleSpider/items.py
@@ -5,9 +5,11 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 import datetime
+import re
 
 import scrapy
-from scrapy.loader.processors import MapCompose, TakeFirst
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+from scrapy.loader import ItemLoader
 
 
 class ArticlespiderItem(scrapy.Item):
@@ -28,22 +30,50 @@ def date_convert(value):
     return create_date
 
 
+# 自定义ItemLoad,重载scrapy自带的ItemLoader
+class ArticleItemLoad(ItemLoader):
+    default_output_processor = TakeFirst()
+
+
+# 正则提取收藏数字
+def get_nums(value):
+    match_re = re.match('.*(\d+).*', value)
+    if match_re:
+        nums = int(match_re.group(1))
+    else:
+        nums = 0
+    return nums
+
+
 # 自定义文章Item
 class JobBoleArticleItem(scrapy.Item):
+    """
     title = scrapy.Field(
         # 传入item时做的预处理参数
         input_processor=MapCompose(lambda x: x + '-jobbole', add_jobbole)
     )
+    """
+    title = scrapy.Field()
     create_date = scrapy.Field(
         input_processor=MapCompose(date_convert),
-        output_processor=TakeFirst()
+        # 此处注释,因为用到了自定义的ItemLoad,里面已经写了输出时的规范,list取出第一行
+        # output_processor=TakeFirst()
     )
     url = scrapy.Field()
     url_object_id = scrapy.Field()
     front_image_url = scrapy.Field()
     front_image_path = scrapy.Field()
-    praise_nums = scrapy.Field()
-    comments_nums = scrapy.Field()
-    fav_nums = scrapy.Field()
-    tags = scrapy.Field()
+    praise_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    comments_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    fav_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    tags = scrapy.Field(
+        #input_processor=MapCompose(get_nums),
+        output_processor=Join(",")
+    )
     content = scrapy.Field()
