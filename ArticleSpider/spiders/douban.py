d84feb4386db7db39409fde8dfa10a1d30c78cb3
unlimitbladeworks <13121347080@sina.cn>
2018-11-08 22:53:39 +0800

    add crawl rule

--- ArticleSpider/spiders/douban.py
+++ ArticleSpider/spiders/douban.py
@@ -5,6 +5,7 @@ import scrapy
 from scrapy import Request
 
 from items import DouBanItem, DouBanItemLoad
+from utils.common import get_md5
 
 
 class DoubanSpider(scrapy.Spider):
@@ -16,9 +17,10 @@ class DoubanSpider(scrapy.Spider):
 
     def parse(self, response):
         """ 交给parse_details 进行处理 """
-        # comment_nodes = response.css('.mod-bd')
-        # for _ in comment_nodes:
-        yield Request(url=response.url, callback=self.parse_details)
+        comment_nodes = response.css('.mod-bd')
+        for comments_iter_nums, _ in enumerate(comment_nodes):
+            yield Request(url=response.url, callback=self.parse_details,
+                          meta={"comments_iter_nums": comments_iter_nums})
 
         # 提取下一页交给scrapy进行下载
         next_page = response.css('.next::attr(href)').extract_first('')
@@ -27,8 +29,18 @@ class DoubanSpider(scrapy.Spider):
 
     def parse_details(self, response):
         """ 解析爱情公寓具体短评论 """
+        i = str(response.meta.get('comments_iter_nums', '') + 1)
         item_loader = DouBanItemLoad(item=DouBanItem(), response=response)
-        item_loader.add_xpath('douban_url', '//*[@id="comments"]/div[1]/div[2]/h3/span[2]/a/@href')
+        item_loader.add_xpath('douban_url', f'//*[@id="comments"]/div[{i}]/div[2]/h3/span[2]/a/@href')
+        user_url = response.xpath(f'//*[@id="comments"]/div[{i}]/div[2]/h3/span[2]/a/@href').extract_first('')
+        item_loader.add_value('url_hashid', get_md5(user_url))
+        item_loader.add_xpath('user_name', f'//*[@id="comments"]/div[{i}]/div[2]/h3/span[2]/a/text()')
+        item_loader.add_xpath('is_view', f'//*[@id="comments"]/div[{i}]/div[2]/h3/span[2]/span[1]/text()')
+        item_loader.add_xpath('star_number', f'//*[@id="comments"]/div[{i}]/div[2]/h3/span[2]/span[2]/@title')
+        item_loader.add_xpath('comment_time', f'//*[@id="comments"]/div[{i}]/div[2]/h3/span[2]/span[3]/text()')
+        item_loader.add_xpath('votes_numbers', f'//*[@id="comments"]/div[{i}]/div[2]/h3/span[1]/span/text()')
+        item_loader.add_xpath('short_comment', f'//*[@id="comments"]/div[{i}]/div[2]/p/span/text()')
+
         # 必须调用此步骤
         douban_item = item_loader.load_item()
         ''' 通过item_loader加载item,目的：比原来的item便于维护 end'''
