fc02449238195585fd36dcbdc7a1f61c2bbf30fe
unlimitbladeworks <13121347080@sina.cn>
2018-11-15 21:24:58 +0800

    add analysis data modual

---
+++ ArticleSpider/analysis/sql_byself.py
@@ -0,0 +1,17 @@
+#coding = utf-8
+
+"""
+@author: sy
+
+@file: sql_byself.py
+
+@time: 2018/11/15 21:14
+
+@desc: 自定义sql
+
+"""
+class SqlBySelf(object):
+
+    def select_comments(self):
+        # todo 待添加查询评论的sql
+        pass
